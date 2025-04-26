b6c76b168d916343cf9a64c4c83160314617810c
piyushga <64128518+piyushga@users.noreply.github.com>
2025-04-26 12:26:05 +0200

    Using live swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,36 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Body = () => {
   //State variable
-  const [listOfResturants, setListOfResturants] = useState(resList);
+  const [listOfResturants, setListOfResturants] = useState([]);
+
+  useEffect(() => {
+    console.log("Use effect Called");
+    data();
+  }, []);
+
+  const data = async () => {
+    const foodData = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.047399&lng=73.0697805&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const resLists = await foodData.json();
+    console.log("Response body: ", resLists);
+
+    console.log(
+      "Resturants Lists: ",
+      resLists?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+
+    setListOfResturants(
+      resLists?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
 
   return (
     <div className="Body">
@@ -13,7 +39,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfResturants.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating > 4.2
             );
 
             console.log("Button click: ", filteredList);
