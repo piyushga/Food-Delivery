3c4e30cd521d0577c2c344dabf1bb5855fb34f87
piyushga <64128518+piyushga@users.noreply.github.com>
2025-05-02 12:06:37 +0200

    added search resturant functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,10 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   //State variable
   const [listOfResturants, setListOfResturants] = useState([]);
+  const [searchResturant, setSearchResturant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
 
   useEffect(() => {
     console.log("Use effect Called");
@@ -31,6 +35,11 @@ const Body = () => {
       resLists?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+
+    setSearchResturant(
+      resLists?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
 
   return listOfResturants.length === 0 ? (
@@ -38,22 +47,45 @@ const Body = () => {
   ) : (
     <div className="Body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+
+              const searchList = listOfResturants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setSearchResturant(searchList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = listOfResturants.filter(
-              (res) => res.info.avgRating > 4.2
+            const topRatedList = listOfResturants.filter(
+              (res) => res.info.avgRating > 4.3
             );
 
-            console.log("Button click: ", filteredList);
-            setListOfResturants(filteredList);
+            console.log("Button click: ", topRatedList);
+            setSearchResturant(topRatedList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfResturants.map((resturant) => (
+        {searchResturant.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
